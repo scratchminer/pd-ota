ae7bbfc91c4dfb1f2094723aa56808fb660652b9
scratchminer <121195139+scratchminer@users.noreply.github.com>
2023-08-28 00:34:59 -0400

    Update genresp.py

--- genresp.py
+++ genresp.py
@@ -1,11 +1,17 @@
+
+from hashlib import md5
 from json import dumps
 from os import env
 from sys import argv
 
+content = None
+with open("static/ota_payload.bin", "rb") as f:
+	content = f.read()
+
 print(dumps({
-  "md5": argv[1],
-  "url": "https://scratchminer.github.io/pd-ota/static/ota_payload.bin",
-  "version": env["VERSION"],
-  "notes": "",
-  "decryption_key": None
+	"md5": md5(content).hexdigest(),
+	"url": "https://scratchminer.github.io/pd-ota/static/ota_payload.bin",
+ 	"version": env["VERSION"],
+	"notes": "",
+	"decryption_key": None
 }))
