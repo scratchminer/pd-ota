b0341616d2c3ade11e063dae31a56763def93b25
scratchminer <121195139+scratchminer@users.noreply.github.com>
2023-08-28 00:53:52 -0400

    Update versioncheck.py

--- versioncheck.py
+++ versioncheck.py
@@ -3,7 +3,7 @@ from requests import get
 
 from sys import argv, exit
 
-FIRMWARE_API = f"https://play.date/api/v2/firmware/?current_version={argv[2]}"
+FIRMWARE_API = f"https://play.date/api/v2/firmware/?current_version=2.0.2"
 
 r = get(FIRMWARE_API, headers={
 	"Authorization": f"Token {argv[1]}"
