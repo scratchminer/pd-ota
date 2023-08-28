6f88617a03fa301334531a1794a21f0ef2eb4bbc
scratchminer <121195139+scratchminer@users.noreply.github.com>
2023-08-27 22:54:47 -0400

    Add the patcher

---
+++ pdpatcher.py
@@ -0,0 +1,96 @@
+# pip install cryptography
+# pip install requests
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from requests import get
+
+from argparse import ArgumentParser
+from base64 import b64decode
+from hashlib import md5
+from os import mkdir
+from shutil import rmtree
+from sys import argv, exit
+from zipfile import ZipFile
+
+FIRMWARE_API = "https://play.date/api/v2/firmware/?current_version=2.0.0"
+
+def quit():
+	try: rmtree(".pdpatcher", ignore_errors=True)
+	except FileNotFoundError: pass
+	exit()
+
+if __name__ == "__main__":
+	parser = ArgumentParser(prog="PDPatcher", description="A Playdate firmware patcher by scratchminer")
+	parser.add_argument("-t", help="access token for a registered device", dest="token", required=True)
+	parser.add_argument("-p", help="unlock key, sometimes called the password, for a registered device", dest="pw", metavar="PASSWORD", required=True)
+	parser.add_argument("--no-patch", "-n", action="store_const", const=True, default=False, help="don't patch the firmware at all, just dump it", dest="no_patch")
+	parser.add_argument("-o", default="pdfw", help="output file", dest="out_file")
+	args = parser.parse_args()
+	
+	try: mkdir(".pdpatcher")
+	except FileExistsError: pass
+	
+	print("Requesting firmware from Panic servers...")
+	r = get(FIRMWARE_API, headers={
+		"Authorization": f"Token {args.token}"
+	})
+	
+	if r.status_code != 200:
+		print("There was an error obtaining the firmware download link. Full response below:")
+		print(r.text)
+		quit()
+	
+	j = r.json()
+	
+	url = j["url"]
+	print(f"Downloading {j['version']}...")
+	s = get(url, stream=True)
+	
+	with open(f".pdpatcher/ota_payload.bin", "wb") as f:
+		for part in s.iter_content(512):
+			f.write(part)
+	
+	print(f"Decrypting key for {j['version']}...")
+	base64_data = b64decode(j["decryption_key"])
+	
+	iv = base64_data[:12]
+	data = base64_data[12:]
+	
+	key_aes = AESGCM(bytes(args.pw, "utf-8"))
+	try: key = key_aes.decrypt(iv, data, None)
+	except:
+		print("The password and access token provided do not match. Please ensure your data is correct.")
+		quit()
+	
+	print(f"Extracting {j['version']}...")
+	with ZipFile(f".pdpatcher/ota_payload.bin", "r") as bundle:
+		pdx_name = bundle.namelist()[0].split("/", 1)[0]
+		bundle.extractall(path=".pdpatcher")
+	
+	print(f"Decrypting {j['version']}...")
+	
+	dec_header = None
+	decrypted = None
+	with open(f".pdpatcher/pdfw", "rb") as pdfw:
+		pdfw.read(4)
+		aes = AESGCM(key)
+		decrypted_file = aes.decrypt(pdfw.read(12), pdfw.read(), None)
+		decrypted = decrypted_file[32:]
+		dec_header = decrypted_file[:32]
+	with open(args.out_file, "wb") as pdfw:
+		if not args.no_patch:
+			print(f"Patching {j['version']}...")
+			# change the function telling whether Lua has system privilege
+			decrypted = decrypted.replace(
+				b"\x02\x4b\x18\x78\x00\xf0\x01\x00\x70\x47",
+				b"\x02\x4b\x18\x78\x40\xf0\x01\x00\x70\x47"
+			)
+			# change the function telling whether C has system privilege
+			decrypted = decrypted.replace(
+				b"\xef\xf3\x05\x80\x00\x28\x1c\xbf\x01\x20\x70\x47\xef\xf3\x14\x80\x10\xf0\x01\x0f\x1a\xbf\x00\x20\x02\xdf\x01\x20\x70\x47",
+				b"\xef\xf3\x05\x80\x00\x28\x1c\xbf\x01\x20\x70\x47\xef\xf3\x14\x80\x10\xf0\x01\x0f\x1a\xbf\x01\x20\x02\xdf\x01\x20\x70\x47"
+			)
+		md5_hash = md5(decrypted).digest()[:8]
+		pdfw.write(dec_header[:24])
+		pdfw.write(md5_hash)
+		pdfw.write(decrypted)
+	quit()
