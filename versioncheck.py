2161ce8859089065587d59369cbd59cccc584b76
scratchminer <121195139+scratchminer@users.noreply.github.com>
2023-08-28 10:54:35 -0400

    Update versioncheck.py

--- versioncheck.py
+++ versioncheck.py
@@ -1,9 +1,14 @@
 # pip install requests
 from requests import get
 
+from json import load
 from sys import argv, exit
 
-FIRMWARE_API = f"https://play.date/api/v2/firmware/?current_version={argv[2]}"
+vsn = None
+with open("update.json", "r") as f:
+	vsn = load(f)["version"]
+
+FIRMWARE_API = f"https://play.date/api/v2/firmware/?current_version={vsn}"
 
 r = get(FIRMWARE_API, headers={
 	"Authorization": f"Token {argv[1]}"
